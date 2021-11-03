cc3759f88774f50ae13f7e9e9012d3dbe34dd4b9
drewandrewd <andrewid10@gmail.com>
2021-11-03 19:36:51 +0500

    Revert "new"
    This reverts commit 7702be67

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -2,23 +2,20 @@ public class List {
 
     private Task toDoList;
 
-    public List(Task toDoList) {
-        this.toDoList = toDoList;
-    }
-
     public Task getToDoList() {
         return toDoList;
     }
 
     public void add(String text) {
+        toDoList = new Task();
         getToDoList().setText(text);
     }
 
     public void toggle(int id) throws Exception {
-        if (id == 1 && toDoList.isDone() == false) {
-            toDoList.setDone(true);
-        } else if (id == 1 && toDoList.isDone() == true) {
-            toDoList.setDone(false);
+        if (id == 1 && toDoList.isDone()) {
+            toDoList.setDone(!toDoList.isDone());
+        } else if (id == 1 && !toDoList.isDone()) {
+            toDoList.setDone(!toDoList.isDone());
         } else {
             throw new Exception();
         }
@@ -29,9 +26,8 @@ public class List {
         if (toDoList.isDone() == true) {
             toggle = "[x]";
         }
-        String printLine = 1 + ". " + toggle + " " + toDoList.getText().trim();
         if (argument.equals("all") || (argument.equals("") && toDoList.isDone() == false)) {
-            System.out.println(printLine);
+            System.out.println(1 + ". " + toggle + " " + toDoList.getText().trim());
         } else if (argument.equals("") && toDoList.isDone() == true) {
             System.out.println("Все задачи выполнены!");
         } else {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,19 @@ public class Main {
 
     public static String findArgument(String line, String command) {
         String argument = "";
-        if (!line.equals(command)) {
+        if (command.equals("add")) {
+            argument = line.substring(3).trim();
+        } else if (!line.equals(command)) {
             argument = line.replace(command, "").trim();
         }
         return argument;
     }
 
-    public static String commands(String line) {
+    public static String findCommands(String line) {
         String command = "";
-        if (line.contains(" ")) {
-            command = line.substring(0, line.indexOf(" "));
+        int spaceIndex = line.indexOf(" ");
+        if (spaceIndex >= 0) {
+            command = line.substring(0, spaceIndex);
         } else {
             command = line;
         }
@@ -22,31 +25,32 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        List list = new List(new Task());
+        List list = new List();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        loop:
-            while (true) {
-                try {
-                    String line = reader.readLine();
-                    String command = commands(line);
-                    switch (command) {
-                        case "add":
-                            list.add(findArgument(line, command));
-                            break;
-                        case "print":
-                            list.print(findArgument(line, command));
-                            break;
-                        case "toggle":
-                            list.toggle(Integer.parseInt(findArgument(line, command)));
-                            break;
-                        case "quit":
-                            break loop;
-                        default:
-                            System.out.println("Неправильная команнда!");
-                    }
-                } catch (Exception e) {
-                    System.out.println("Ошибка ввода идентификатора!");
+        boolean loop = true;
+        while (loop) {
+            try {
+                String line = reader.readLine();
+                String command = findCommands(line);
+                switch (command) {
+                    case "add":
+                        list.add(findArgument(line, command));
+                        break;
+                    case "print":
+                        list.print(findArgument(line, command));
+                        break;
+                    case "toggle":
+                        list.toggle(Integer.parseInt(findArgument(line, command)));
+                        break;
+                    case "quit":
+                        loop = false;
+                        break;
+                    default:
+                        System.out.println("Неправильная команнда!");
                 }
+            } catch (Exception e) {
+                System.out.println("Ошибка ввода идентификатора!");
             }
+        }
     }
 }
