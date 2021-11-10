f039c3e1fef784735156a9872a7f621ef9abc7b0
drewandrewd <andrewid10@gmail.com>
2021-11-10 17:21:22 +0500

    Revert "new"
    This reverts commit 7702be67

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-
-    public static String findArgument(String line, String command) {
-        String argument = "";
-        if (command.equals("add")) {
-            argument = line.substring(3).trim();
-        } else if (!line.equals(command)) {
-            argument = line.replace(command, "").trim();
-        }
-        return argument;
-    }
-
-    public static String findCommands(String line) {
-        String command = "";
-        int spaceIndex = line.indexOf(" ");
-        if (spaceIndex >= 0) {
-            command = line.substring(0, spaceIndex);
-        } else {
-            command = line;
-        }
-        return command;
-    }
-
     public static void main(String[] args) {
-        List list = new List();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        boolean loop = true;
-        while (loop) {
+        Lists lists = new Lists(new ArrayList<>());
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
             try {
-                String line = reader.readLine();
-                String command = findCommands(line);
-                switch (command) {
-                    case "add":
-                        list.add(findArgument(line, command));
-                        break;
-                    case "print":
-                        list.print(findArgument(line, command));
-                        break;
-                    case "toggle":
-                        list.toggle(Integer.parseInt(findArgument(line, command)));
-                        break;
-                    case "quit":
-                        loop = false;
-                        break;
-                    default:
-                        System.out.println("Неправильная команнда!");
+                String command = scanner.next();
+                if (command.equals("add")) {
+                    lists.add(scanner.nextLine());
+                } else if (command.equals("print")) {
+                    String id = scanner.next();
+                    lists.print(id);
+                } else if (command.equals("toggle")) {
+                    lists.toggle(Integer.parseInt(scanner.next()));
+                } else if (command.equals("quit")) {
+                    break;
+                } else if (command.equals("delete")) {
+                    lists.delete(Integer.parseInt(scanner.next()));
+                } else if (command.equals("edit")) {
+                    lists.edit(Integer.parseInt(scanner.next()), scanner.nextLine());
+                } else if (command.equals("search")) {
+                    lists.search(scanner.nextLine());
+                } else {
+                    System.out.println("Неправильная команнда!");
                 }
             } catch (Exception e) {
                 System.out.println("Ошибка ввода идентификатора!");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,12 @@
 public class Task {
 
     private String text;
-    private boolean done = false;
+    private boolean done;
+    private String toggle;
 
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
+    public Task(String text) {
+        this.text = text;
+        this.done = false;
     }
 
     public String getText() {
@@ -19,4 +17,20 @@ public class Task {
         this.text = text;
     }
 
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getToggle() {
+        if (isDone() == true) {
+            toggle = "[x]";
+        } else {
+            toggle = "[ ]";
+        }
+        return toggle;
+    }
 }
