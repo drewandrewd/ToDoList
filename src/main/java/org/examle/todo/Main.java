a6da8ded274efea20d6faa6283b466e691faae2e
drewandrewd <andrewid10@gmail.com>
2021-12-09 11:32:06 +0500

    new

--- src/main/java/org/examle/todo/Main.java
+++ src/main/java/org/examle/todo/Main.java
@@ -2,23 +2,30 @@ package org.examle.todo;
 
 import org.examle.todo.model.Lists;
 import org.examle.todo.parser.CommandParser;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
 
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+	public static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
 
-    public static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        Lists lists = Lists.getInstance();
-        CommandParser commandParser = CommandParser.getInstance();
-        while (true) {
-            try {
-                String command = scanner.next();
-                commandParser.parser(command);
-            } catch (Exception e) {
-                Lists.getLogger().error("Ошибка ввода идентификатора!", e);
-            }
-        }
-    }
+	@Override
+	public void run(String... args) {
+		Lists lists = Lists.getInstance();
+		CommandParser commandParser = CommandParser.getInstance();
+		while (true) {
+			try {
+				String command = scanner.next();
+				commandParser.parser(command);
+			} catch (Exception e) {
+				Lists.getLogger().error("Ошибка ввода идентификатора!", e);
+			}
+		}
+	}
 }
